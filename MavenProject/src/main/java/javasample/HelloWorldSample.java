225d393226e5a1d9c6dd15d5f3926b0bc97dbece
Vismitha <Vismitha@LAPTOP-36QH3DNI>
2024-01-25 21:10:59 +0530

    First commit

---
+++ MavenProject/src/main/java/javasample/HelloWorldSample.java
@@ -0,0 +1,10 @@
+package javasample;
+
+public class HelloWorldSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("HelloWorld");
+	}
+
+}
